37240d532afa6a7bc576fd7d89697904482a1958
t33k00 <tmkyllonen@gmail.com>
2022-10-11 15:40:37 +0300

    ..

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,20 +1,21 @@
-import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-function App() {
-  const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-  const ICON_URL = 'https://api.openweathermap.org/img/wn/';
-  const API_KEY = '';
-  const [latitude, setLatitude] = useState(0)
-  const [longtitude, setLongtitude] = useState(0)
-  const [isloading, setisloading] = useState(false)
-  const [temp, settemp] = useState(0)
-  const [speed, setspeed] = useState(0)
-  const [direction, setdirection] = useState(0)
-  const [description, setdescription] = useState(0)
-  const [icon, seticon] = useState(0)
 
-  useEffect(() => {
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const ICON_URL = 'https://api.openweathermap.org/img/wn/';
+const API_KEY = '';
+
+export default function weather({latitude,longitude}){
+const [latitude, setLatitude] = useState(0)
+const [longtitude, setLongtitude] = useState(0)
+const [isloading, setisloading] = useState(false)
+const [temp, settemp] = useState(0)
+const [speed, setspeed] = useState(0)
+const [direction, setdirection] = useState(0)
+const [description, setdescription] = useState(0)
+const [icon, seticon] = useState(0)
+
+useEffect(() => {
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition(position=>{
         console.log(position)
@@ -51,19 +52,15 @@ function App() {
 
 
   }, [])
-  if (isloading!=0){
-    return <p>ladataan sijaintia...</p>
-  } else {
-    return(
+
+  return(
     <p>Sijaintisi:{latitude},{longtitude}
-    <weather latitude={latitude}, longitude={longtitude} />
-    </p>
-    
-    
-  )}
-
-
- 
+        <>
+    <h3>Weather at your location</h3>
+    <p>{temp} C&#176;</p>
+    <p>{speed} m/s{direction} degrees</p>
+    <p>{description}</p>
+    <img src={icon} alt=""/>
+    </></p>
+  )
 }
-
-export default App;
